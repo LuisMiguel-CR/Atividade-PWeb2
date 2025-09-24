787121bd9903a48c287f61e131510c079bc5e0cc
Luís Miguel Carvalho da Rocha <luis.carvalho@academico.ifpb.edu.br>
2025-09-24 17:33:49 -0300

    Create lib.js
    In[ício da resolução

---
+++ lib.js
@@ -0,0 +1,31 @@
+//Exercício 1
+function area_circulo(raio) {
+    return 3.141592653589793*(raio**2)
+}
+
+// Exercício 2
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+function sub(num1, num2) {
+    return num1 - num2
+}
+
+function mult(num1, num2) {
+    return num1*num2
+}
+
+function div(num1, num2) {
+    if (num2 != 0) {
+        return num1/num2
+    } else {
+        console.log('Não é possível executar divisões por 0')
+    }
+}
+
+//Exercício 3
+
+function e_triangulo(a, b, c) {
+    
+}
