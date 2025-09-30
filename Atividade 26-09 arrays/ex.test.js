02ce317ac331f8a44d2d7ac49012bfe5c04b6a22
Luis Miguel Carvalho <Miguel@pmbex.local>
2025-09-30 10:29:28 -0300

    Finalização dos exercícios 8 e 12 sobre arrays

--- Atividade 26-09 arrays/ex.test.js
+++ Atividade 26-09 arrays/ex.test.js
@@ -1,6 +1,6 @@
 import {it, test} from 'node:test';
 import assert from 'node:assert';
-import {sum, sumOdds, product} from './lib.js'
+import {sum, sumOdds, product, utilitarios} from './lib.js'
 
 test('Teste exercício 8', () => {
     it('Deve somar os números de um vetor', () => {
@@ -24,5 +24,28 @@ test('Teste exercício 8', () => {
 })
 
 test('Teste exercício 12', () => {
-    
-})
+    it('Retornar o menor número da lista', () => {
+        assert.strictEqual(utilitarios('min', [1, 4, 2, 6, 10, 3]), 1);
+        assert.strictEqual(utilitarios('min', [1, 4, -1, 6, 10, 3]), -1);
+    })
+    it('Retornar o maior valor da lista', () => {
+        assert.strictEqual(utilitarios('max', [1, 4, 2, 6, 10, 3]), 10);
+    })
+    it('Criar uma lista nova a partir de determinados parâmetros'), () => {
+        assert.strictEqual(utilitarios('range',[10]), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert.strictEqual(utilitarios('range',[1 ,11]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        assert.strictEqual(utilitarios('range',[0, 30, 5]), [0, 5, 10, 15, 20, 25]);
+    }
+    it('Concatena as listas', () => {
+        assert.deepStrictEqual(utilitarios('zip',[['moe', 'larry'], [30, 40]]), [['moe', 30], ['larry', 40]]);
+        assert.deepStrictEqual(utilitarios('zip', [['moe', 'larry', 'curly'] , [30, 40, 50], [true, false, false]]), [['moe', 30, true], ['larry', 40, false], ['curly', 50, false]])
+    })
+    it('Remover repetições de uma lista', () => {
+        assert.deepStrictEqual(utilitarios('uniq', [1, 2, 1, 4, 1, 3]), [1, 2, 3, 4])
+        assert.deepStrictEqual(utilitarios('uniq', [1, 2, 1, 3, 3]), [1, 2, 3])
+    })
+    it('Organizar a lista em ordem crescente', () => {
+        assert.deepStrictEqual(utilitarios('sortNum', [1, 3, 2]), [1, 2, 3])
+        assert.deepStrictEqual(utilitarios('sortNum', [1, 2, 10, 3, 32]), [1, 2, 3, 10, 32])
+    })
+    })
--- Atividade 26-09 arrays/lib.js
+++ Atividade 26-09 arrays/lib.js
@@ -30,22 +30,55 @@ export { sum, sumOdds, product };
 
 //Exercício 12
 
-function utilitarios(operacao, lista, lista1, lista2) {
+function utilitarios(operacao, lista) {
     switch (operacao) {
         case 'min':
-            lista.sort()
-            return lista[0]
+            return Math.min(...lista)
         case 'max':
-            lista.sort()
-            lista.reverse()
-            return lista[0]
+            return Math.max(...lista)
         case 'range':
             lista_nova = []
-            for (const i = 0; i < lista; i++ ) {
+            if (typeof lista === 'number') {
+                for (const i = 0; i < lista; i++ ) {
                 lista_nova[i] = i
+                }
+                return lista_nova
+            } if (lista.length() === 2) {
+                for (const i = lista[0]; i < lista[1]; i++){
+                    lista_nova.push(i)
+                }
+                return lista_nova
+            } if (lista.length() === 3) {
+                for (const i = lista[0]; i < lista[1]; i += lista[2]) {
+                    lista_nova.push(i)
+                }
+                return lista_nova
             }
-            return lista_nova
         case 'zip':
+            const quant_listas = lista.length;
+            if (quant_listas === 2) {
+                const nova_lista = [[lista[0][0],lista[1][0]], [lista[0][1] , lista[1][1]]]
+                return nova_lista
 
+            } if (quant_listas === 3) {
+                const nova_lista = [[lista[0][0], lista[1][0], lista[2][0]], [lista[0][1], lista[1][1], lista[2][1]], [lista[0][2], lista[1][2], lista[2][2]]]
+                return nova_lista
+            }
+        case 'uniq':
+            lista.sort()
+            let nova_lista = []
+            for (const item of lista) {
+                if (nova_lista.includes(item) != true) {
+                    nova_lista.push(item)
+                }
+            }
+            return nova_lista
+        case 'sortNum':
+            lista.sort((a, b) => a - b)
+            return lista
     }
 }
+
+export {utilitarios};
+
+console.log(utilitarios('max', [1, 4, 2, 6, 10, 3]))
